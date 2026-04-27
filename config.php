966b8fbfaea61ef3027a0e45472e3b3d702681c6
checchiv <87395183+Checchivin@users.noreply.github.com>
2026-04-27 09:57:37 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST','localhost');
+define('DB_HOST','mariadb');
 define('DB_USER','root');
-define('DB_PASS','');
+define('DB_PASS','S@il0rM00n132*');
 define('DB_NAME','poketop');
 ?>
